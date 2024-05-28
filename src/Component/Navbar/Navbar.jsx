f92fe94c48559ecad690a0e25fe7b91ae2094f1c
Bala <65647767+Bala-Subramanian@users.noreply.github.com>
2024-05-28 11:34:30 +0530

    To do changes

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,30 +1,56 @@
 import React from "react";
 import "./Navbar.css";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
-function Navbar(){
-return(
-    <div>
-        <h1>MY APP</h1>
-        <div className="options">
-            <Link className="option" to="/home">Home</Link>
-            <Link className="option" to="/about">About</Link>
-            <Link className="option" to="/contact">Contact</Link>
-            <Link className="option" to="/"></Link>
+function Navbar({ isAuthenticated, username }){
+    let screenName = "Login";
+    let isLoginpage  = true;
+    const location = useLocation();
+    if (location.pathname.startsWith('/blog/')) {
+        screenName = 'Blog Post';
+        isLoginpage  = false;
+    } else if (location.pathname.startsWith('/profile/')) {
+        screenName = 'User Profile';
+        isLoginpage  = false;
+    } else if (location.pathname === '/about') {
+        screenName = 'About Us';
+        isLoginpage  = false;
+    } else if (location.pathname === '/contact') {
+        screenName = 'Contact Us';
+        isLoginpage  = false;
+    }else if (location.pathname === '/login') {
+        screenName = 'Login';
+        isLoginpage  = true;
+    }else if (location.pathname.startsWith('/home/')) {
+        screenName = 'Home';
+        isLoginpage  = false;
+    }else{
+        screenName = 'Home';
+        isLoginpage  = false;
+    }
+    return(
+    <div className="navbar">
+        <div>
+            <div className="navbar-heading">MY APP</div>
+            <div className="navbar-subheading">
+                React training application.<b> {screenName} Screen</b>
+            </div>
+        </div>
+        
+        <div className="navbar-links">
+            {!isLoginpage &&<Link to="/home/:username">Home</Link>}
+            {!isLoginpage &&<Link to="/about">About</Link>}
+            {!isLoginpage &&<Link to="/contact">Contact</Link>}
+            {!isLoginpage &&<Link to="/blog/1">Blog Post 1</Link>}
+            {!isLoginpage &&<Link to="/blog/2">Blog Post 2</Link>}
+            {isAuthenticated  &&<Link to="/profile/:username">User Profile</Link>}
+            {!isLoginpage && 
+                <Link style={ {backgroundColor:"red", color:"#fff", padding:"4px" }} to="/login">Logout</Link>
+            }
+            {!isLoginpage && <Link to="/"></Link>}
         </div>
         {/* <Outlet/> */}
     </div>
     )
 }
 export default Navbar;
-
-// return(
-//     <nav>
-//     <div className="topnav">
-//         <a className="active" href="home">Home</a>
-//         <a href="about">About</a>
-//         <a href="#contact">Contact</a>
-//     </div>
-//     </nav>
-
-//     )
